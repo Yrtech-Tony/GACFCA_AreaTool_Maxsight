f94df180d7322da9472a3d58df1f5b0e1de4a19d
moujunsheng <moujunsheng15@163.com>
2018-02-01 14:28:46 +0800

    添加了PlansPositionDto

---
+++ FIAT.API/src/FIAT.API/Models/Tour/PlansPositionDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FIAT.API.Models.Tour
+{
+    public class PlansPositionDto
+    {
+        public int Id { get; set; }
+        public int Batch { get; set; }
+        public int DisId { get; set; }
+        public string SalesConsultant { get; set; }
+        public string SalesManager { get; set; }
+        public string SalesInside { get; set; }
+        public string InUserid { get; set; }
+        public DateTime InDateTime { get; set; }
+        public string ModifyUserId { get; set; }
+        public DateTime ModifyDatetime { get; set; }
+    }
+    
+
+}
