2075239fc06a587150000ecf80c02c3f64675a15
Tony <moujunsheng15@163.com>
2018-03-06 22:24:35 +0800

    添加了区域的Dto

---
+++ FIAT.API/src/FIAT.API/Models/UsersDto/DistributorDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FIAT.API.Models.DistributorDto
+{
+    public class DistributorDto
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Type { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string ZoneType { get; set; }
+        public bool UseYN { get; set; }
+        public int InUserId { get; set; }
+        public DateTime InDateTime { get; set; }
+        public string UserName { get; set; }// 登录账号
+    }
+}
