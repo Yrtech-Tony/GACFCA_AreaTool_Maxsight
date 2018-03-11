3287e6e4a2478b41049f2e5d9052258192cf8399
Tony <moujunsheng15@163.com>
2018-03-11 22:11:28 +0800

    添加了区域信息

--- FIAT.API/src/FIAT.API/Models/ReportDto/ResultDto.cs
+++ FIAT.API/src/FIAT.API/Models/ReportDto/ResultDto.cs
@@ -7,7 +7,8 @@ namespace FIAT.API.Models.ReportDto
 {
     public class ResultDto
     {
-        public ResultDto(){
+        public ResultDto()
+        {
             RAPList = new List<RAProgressDto>();
             RATCList = new List<RATCScoreDto>();
             STCList = new List<STCScoreDto>();
@@ -39,12 +40,29 @@ namespace FIAT.API.Models.ReportDto
         public string ATScore { get; set; }
         public string DType { get; set; }
     }
-    public class STCScoreDto{
+    public class STCScoreDto
+    {
         public string ItemScore { get; set; }
         public string TCId { get; set; }
         public string SDisId { get; set; }
         public string SDisName { get; set; }
         public string SDisCode { get; set; }
+        public string SmallAreaId { get; set; }
+
+        public string SmallAreaCode { get; set; }
+
+        public string SmallAreaName { get; set; }
+
+        public string MidAreaId { get; set; }
+
+        public string MidAreaCode { get; set; }
+
+        public string MidAreaName { get; set; }
+
+        public string BigAreaId { get; set; }
+
+        public string BigAreaCode { get; set; }
+        public string BigAreaName { get; set; }
         public string AreaId { get; set; }
         public string STScore { get; set; }
     }
